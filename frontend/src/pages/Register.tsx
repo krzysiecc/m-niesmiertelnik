6081f4e039202d66578e4599e90d2eecff34b830
OskarW04 <oskarwitek04@gmail.com>
2025-10-12 07:38:35 +0200

    fix: now will work :)))

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { Input } from '../components/forms/Input';
+import { Input } from '../components/forms/input';
 
 export default function Register() {
   const navigate = useNavigate();
