a426e79f641ab29e4d6f15e0b282ce49a9eae5c2
OskarW04 <oskarwitek04@gmail.com>
2025-10-12 07:59:15 +0200

    fix: config again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ['iteracja-hackathon-1110-2.onrender.com'],
+  }
 });
